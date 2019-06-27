1e355ad07f42520fd9f924e5c3bf237901bb16cd
Israel1Nicolau <45297037+Israel1Nicolau@users.noreply.github.com>
2019-06-27 18:46:49 -0300

    Add files via upload

--- condAninhadas007.py
+++ condAninhadas007.py
@@ -1,36 +1,40 @@
 from random import randint
-
 cores = {'limpa': '\033[m',
          'negrito': '\033[1m',
-         'azul negrito': '\033[1;34m',
-         'verde negrito': '\033[1;32m',
-         'vermelho negrito': '\033[1;31m'}
+         'azul negrito': '\033[1;34m'}
 
-print('{}-='.format(cores['negrito'])*10, 'PREÇO NORMAL DO PRODUTO', '-='*10, '{}'.format(cores['limpa']))
+print('{}{:=^40}'.format(cores['negrito'], 'LOJAS LOJAS'))
 preco = randint(200, 250)
 
-print(' '*25, '{}R${}{}'.format(cores['azul negrito'], preco, cores['limpa']), ' '*25)
+print('{}O preço do seu produto: R${}'.format(cores['azul negrito'],preco))
 print('{}ESCOLHA A OPÇÃO DE PAGAMENTO:{}\n'
       '1 - à vista dinheiro/cheque: 10% de desconto\n'
       '2 - à vista no cartão: 5% de desconto\n'
       '3 - em até 2x no cartão: preço normal\n'
-      '4 - 3x ou mais no cartão: 20% de juros'.format(cores['negrito'], cores['limpa']))
-print('{}-='.format(cores['negrito'])*20, '{}'.format(cores['limpa']))
-
+      '4 - 3x ou mais no cartão: 20% de juros\n'.format(cores['negrito'], cores['limpa']))
+print('{}{:=^40}{}'.format(cores['negrito'], '', cores['limpa']))
 opcao = int(input('Qual opção você prefere? '))
 
 if opcao == 1:
     novo_preco = preco - (preco*0.10)
+    print('O valor do produto é R${}\n'
+          'E com os 10% de desconto ficará por R${}'.format(preco, novo_preco))
 elif opcao == 2:
     novo_preco = preco - (preco*0.05)
+    print('O valor do produto é R${}\n'
+          'E com os 5% de desconto ficará por R${}'.format(preco, novo_preco))
 elif opcao == 3:
     novo_preco = preco
-else:
+    print('O valor do produto é R${}\n'
+          'Com a opção escolhida, o valor final do produto será o mesmo.'.format(preco))
+elif opcao == 4:
     novo_preco = preco + (preco*0.20)
-
-print('{}-='.format(cores['negrito'])*20, '{}'.format(cores['limpa']))
-print('A opcao de pagamento foi de número {}{}{}\n'
-      'O valor do produto é {}R${}{}\n'
-      'E com a opção de pagamento ficará por {}R${}{}'.format(cores['negrito'], opcao, cores['limpa'], cores['verde negrito'], preco, cores['limpa'], cores['vermelho negrito'] ,novo_preco, cores['limpa']))
-print('{}-='.format(cores['negrito'])*20, '{}'.format(cores['limpa']))
-
+    tot_parc = int(input('Quantas parcelas? '))
+    if tot_parc < 3:
+        print('Inválido. Tente novamente')
+    else:
+        parcela = novo_preco/tot_parc
+        print('Sua compra será parcelada em {}x de R${:.2f} com juros'.format(tot_parc, parcela))
+else:
+    novo_preco = 0
+    print('Opção inválida. Tente novamente')
